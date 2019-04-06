cd1886b70e54261c61388703ed44af0d8f17f1df
igorick <studente.gori.michele@gmail.com>
2019-04-06 11:16:40 +0200

    aggiunta metodo costruttore vuoto cliente

---
+++ Cliente.java
@@ -0,0 +1,15 @@
+
+public class Cliente {
+	private String ID;
+	private String Cognome;
+	private String Nome;
+	private String Telefono;
+	private int gradoSoddisfazione;
+	
+	public Cliente() {
+		
+	}
+	
+	
+	
+}
